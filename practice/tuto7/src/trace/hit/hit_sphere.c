9be7ea78811401708456a4595eb6eee55b3ce404
김재환 <woghks7859@gmail.com>
2022-10-20 19:56:13 +0900

    STUDY: tuto7

---
+++ practice/tuto7/src/trace/hit/hit_sphere.c
@@ -0,0 +1,39 @@
+#include "../../../include/structures.h"
+#include "../../../include/utils.h"
+#include "../../../include/trace.h"
+
+t_bool  hit_sphere(t_object *world, t_ray *ray, t_hit_record *rec)
+{
+    t_vec3 oc;
+    double a;
+    double half_b;
+    double c;
+    double discriminant;
+    double sqrtd;
+    double root;
+    t_sphere *sp;
+
+    sp = world->element;
+    oc = vminus(ray->origin, sp->center);
+    // vdot 을 vlength2 무관한 식
+    a = vlength2(ray->dir);
+    half_b = vdot(oc, ray->dir);
+    c = vlength2(oc) - sp->radius2;
+    discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0)
+        return (FALSE);
+    sqrtd = sqrt(discriminant);
+    root = (-half_b - sqrtd) / a;
+    if (root < rec->tmin || rec->tmax < root)
+    {
+        root = (-half_b + sqrtd) / a;
+        if (root < rec->tmin || rec->tmax < root)
+            return (FALSE);
+    }
+    rec->t = root;
+    rec->p = ray_at(ray, root);
+    rec->normal = vdivide(vminus(rec->p, sp->center), sp->radius);
+    set_face_normal(ray, rec);
+    return (TRUE);
+}
