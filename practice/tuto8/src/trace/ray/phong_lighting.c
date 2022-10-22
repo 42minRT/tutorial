31f769f21b9532077f6cd8b42af841b1b854bcc9
김재환 <woghks7859@gmail.com>
2022-10-22 12:37:57 +0900

    STUDY: tuto8 phong lighting model ambient & diffuse

---
+++ practice/tuto8/src/trace/ray/phong_lighting.c
@@ -0,0 +1,30 @@
+#include "../../../include/trace.h"
+
+t_color3    phong_lighting(t_scene *scene)
+{
+    t_color3 light_color;
+    t_object *lights;
+
+    light_color = color3(0, 0, 0);
+    lights = scene->light;
+    while (lights)
+    {
+        if (lights->type == LIGHT_POINT)
+            light_color = vplus(light_color, point_light_get(scene, lights->element));
+        lights = lights->next;
+    }
+    light_color = vplus(light_color, scene->ambient);
+    return (vmin(vmult_(light_color, scene->rec.albedo), color3(1, 1, 1)));
+}
+
+t_color3    point_light_get(t_scene *scene, t_light *light)
+{
+    t_color3 diffuse;
+    t_vec3 light_dir;
+    double kd; // diffuse 의 강도
+
+    light_dir = vunit(vminus(light->origin, scene->rec.p));
+    kd = fmax(vdot(scene->rec.normal, light_dir), 0.0);
+    diffuse = vmult(light->light_color, kd);
+    return (diffuse);
+}
